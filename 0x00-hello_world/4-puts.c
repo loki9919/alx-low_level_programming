e544e758f4051c28b5a247214b0a052feb68226f
lokmane rouijel <lokmane9919@gmail.com>
2023-09-07 16:51:29 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - a program that prints a line with puts function
+ * return : 0 (success)
+ */
+int imain(void)
+{
+	puts("\"Programming is like building a multilingual puzzle/n");
+	return (0);
+}
