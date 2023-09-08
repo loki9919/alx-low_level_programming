17e6da436199cb1c5ed9b83711b29297d982fb90
lokmane rouijel <lokmane9919@gmail.com>
2023-09-07 19:14:37 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - this main function
  * Return: 1
  */
 int main(void)
 {
-	putchar('and that piece of art is useful" - Dora Korpar, 2015-10-19\n');
+	write('and that piece of art is useful" - Dora Korpar, 2015-10-19\n');
 	return (1);
 }
