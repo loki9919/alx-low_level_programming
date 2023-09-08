f5f9097c05735070717a2517a6438b11596c956c
lokmane rouijel <lokmane9919@gmail.com>
2023-09-07 19:07:57 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this main function
+ * Return: 1
+ */
+int main(void)
+{
+	putchar('and that piece of art is useful" - Dora Korpar, 2015-10-19\n');
+	return (1);
+}
