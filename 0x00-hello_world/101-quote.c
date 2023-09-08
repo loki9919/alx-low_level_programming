f56d6b37d165fc85b09e1fbfd10aa37cf74e0489
lokmane rouijel <lokmane9919@gmail.com>
2023-09-07 19:38:27 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <unistd.h>
+#include <string.h>
 /**
- * main - this main function
+ * main - this main function prints a message and returns 1
  * Return: 1
  */
 int main(void)
 {
-	write(1, 'and that piece of art is useful" - Dora Korpar, 2015-10-19\n', 13);
+	const char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	size_t length = strlen(quote);
+	write(1, quote, lentgh);
 	return (1);
 }
