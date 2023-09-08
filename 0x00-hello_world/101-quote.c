34ede4472ae0ef2c490e59e12b94490225ce976b
lokmane rouijel <lokmane9919@gmail.com>
2023-09-07 19:45:21 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,8 @@
 #include <unistd.h>
-
+/**
+ * main - this main function return 1
+ * Return: 1
+ */
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
