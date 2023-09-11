b0e82709cf703bf6423c6291b04d9cc5e3d63483
lokmane rouijel <lokmane9919@gmail.com>
2023-09-11 16:08:11 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is main function
+ * Return: always 0
+*/
+int main(void)
+{
+	unsigned char N = '0';
+
+	while (N <= '9')
+{
+	putchar(N);
+	N++;
+}
+	putchar('\n');
+	return (0);
+}
