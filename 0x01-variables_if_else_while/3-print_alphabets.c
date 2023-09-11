e0fe4d9557361683dd69aa788298a452f2cab609
lokmane rouijel <lokmane9919@gmail.com>
2023-09-10 22:37:50 -0700

    print low and upercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * Return: always 0
+*/
+int main(void)
+{
+        char lowercase = 'a';
+	char upercase = 'A';
+
+        while (lowercase <= 'z')
+{
+        putchar(lowercase);
+        lowercase++;
+}
+	while (upercase <= 'Z')
+{
+	putchar(upercase);
+	upercase++;
+}
+        putchar('\n');
+        return (0);
+}
