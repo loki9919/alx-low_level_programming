5ad6d8a87b4cf256ebb8d1b1259afc847def3c87
lokmane rouijel <lokmane9919@gmail.com>
2023-09-10 23:04:26 -0700

    prnt alphabet !=e,q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,17 @@ int main(void)
 {
 	char lowercase = 'a';
 
-	while (lowercase <= 'z' || lowercase != 'q' || lowercase != 'e')
+	while (lowercase <= 'z')
 {
 	putchar(lowercase);
 	lowercase++;
+	if (lowercase == 'e')
+	{
+	putchar('\0')
+}
+	if (lowercase == 'q')
+	{
+	putchar('\0')
 }
 	putchar('\n');
 	return (0);
