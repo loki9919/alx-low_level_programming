f79ed99eac39a8cd4a72df4d093213c6fe263573
lokmane rouijel <lokmane9919@gmail.com>
2023-09-11 16:39:36 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - this is main function
+ * Return: always 0
+*/
+int main(void)
+{
+	int num = 0;
+	while (num < 10)
+	{
+		putchar(num + '0');
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
