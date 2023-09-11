427c15fe4bf0a048e3246adf577c07398eb4089a
lokmane rouijel <lokmane9919@gmail.com>
2023-09-10 21:12:20 -0700

    negative or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* i include the <stdio.h> for printf command */
 
-/* main - this is main function */
+/**
+ * main - this is main function
+ * Return: always 0
+*/
 int main(void)
 {
 	int n;
