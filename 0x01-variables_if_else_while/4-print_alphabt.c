fd1ed973c260187fecbfa9c1039aca22c57035a1
lokmane rouijel <lokmane9919@gmail.com>
2023-09-10 22:50:31 -0700

    prnt alphabet !=e,q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * Return: always 0
+*/
+int main(void)
+{
+	char lowercase = 'a';
+
+	while (lowercase <= 'z' && lowercase != q && lowercase != e)
+{
+	putchar(lowercase);
+	lowercase++;
+}
+	putchar('\n');
+	return (0);
+}
