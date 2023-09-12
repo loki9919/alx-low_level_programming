f02ae12ab59acbe02e2c70150d8ef0a3ec2f3d22
lokmane rouijel <lokmane9919@gmail.com>
2023-09-12 10:15:24 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio>
+
+/**
+ * main- this is the main function
+ * Return: always 0
+*/
+int main(void)
+{
+	int _putchar
+	
+	{
+		putchar(_putchar);
+	}
+	Putchar('\n');
+	return (0);
+}
