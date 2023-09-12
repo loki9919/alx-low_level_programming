00350cc5a17cc275955414bc31ff31749b9bdd91
lokmane rouijel <lokmane9919@gmail.com>
2023-09-12 16:00:03 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) - similair to the islower function
+*/
+int _islower(int c);
+
+{
+	char ch;
+
+	if (ch = 'c')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
