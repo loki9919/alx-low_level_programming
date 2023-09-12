a0a31adefce36e0b73c06c444476bc05ac14205e
lokmane rouijel <lokmane9919@gmail.com>
2023-09-12 11:52:07 -0700

    prnt alph

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main1.h"
+
+/**
+ * main - this is the main function
+ * Return: always 0
+*/
+int main(void)
+{
+	print_alphabet(void);
+	return (0);
+	}
+	void print_alphabet(void)
+	char alph = 'a'
+{
+	while (alph <= 'z')
+	{
+		putchar("%c", alph);
+	}
+	putchar('\n');
+}
