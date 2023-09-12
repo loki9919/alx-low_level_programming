bae014b3cdb60716c29874e0e1185c8168c5045b
lokmane rouijel <lokmane9919@gmail.com>
2023-09-11 19:42:30 -0700

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,30 +8,30 @@ int main(void)
 {
 	unsigned int first_digit = 0;
 
-		while (first_digit < 8)
-		{
-			unsigned int second_digit = first_digit + 1;
+	while (first_digit < 8)
+	{
+		unsigned int second_digit = first_digit + 1;
 
-				while (second_digit <= 8)
+		while (second_digit <= 8)
+			{
+			unsigned int third_digit = second_digit + 1;
+
+			while (third_digit <= 9)
+			{
+				putchar(first_digit + '0');
+				putchar(second_digit + '0');
+				putchar(third_digit + '0');
+				if (first_digit != 7)
 				{
-					unsigned int third_digit = second_digit + 1;
-
-					while (third_digit <= 9)
-						{
-							putchar(first_digit + '0');
-							putchar(second_digit + '0');
-							putchar(third_digit + '0');
-						if (first_digit != 7)
-							{
-								putchar (',');
-									putchar (' ');
-							}
-							third_digit++;
-						}
-						second_digit++;
+					putchar (',');
+					putchar (' ');
 				}
-				first_digit++;
+				third_digit++;
+			}
+			second_digit++;
 		}
-		putchar('\n');
-		return (0);
+		first_digit++;
+	}
+	putchar('\n');
+	return (0);
 }
