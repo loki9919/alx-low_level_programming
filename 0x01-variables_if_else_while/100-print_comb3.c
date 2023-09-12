ddbaaac556b12f8c74f9f324ac4f70da1f3bc965
lokmane rouijel <lokmane9919@gmail.com>
2023-09-11 19:11:16 -0700

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: always 0
+*/
+int main(void)
+{
+	unsigned int frist_digit = 0;
+
+	while (first_digit < 9)
+	{
+		unsigned int second_digit = first_digit + 1;
+
+		while (second_digit <= 9)
+		{
+			putchar(first_digit + '0');
+			putchar(second_digit + '0');
+			if (firt_digit != 8)
+			{
+				putchar(",");
+				putchar(" ");
+			}
+			second_digit++;
+		}
+		first_digit++;
+	}
+	putchar(\n);
+	return (0);
+}
