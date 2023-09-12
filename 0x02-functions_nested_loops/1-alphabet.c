4741fae5faa79add366845134b855452031fef58
lokmane rouijel <lokmane9919@gmail.com>
2023-09-12 13:19:44 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,14 @@
 /**
  * main - this is the main function
  * Retur: always 0
- * print_alphabet - to print an alphabet
 */
 
 void print_alphabet(void)
 
 {
 	char letter = 'a';
+	
 	while (letter <= 'z')
-
 	{
 		_putchar(letter);
 		letter++;
