374aa75ab5fd1ff16e7fbc65856362641f27765b
lokmane rouijel <lokmane9919@gmail.com>
2023-09-12 16:58:03 -0700

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,15 +8,15 @@ int print_sign(int n)
 {
 	if (n >= 0)
 	{
-		_putchar(+, 1);
+		_putchar('+', 1);
 	}
 	else if (n == 0)
 	{
-		_putchar(0, 0);
+		_putchar('0', 0);
 	}
 	else
 	{
-		_putchar(-, /);
+		_putchar('-', /);
 	}
 	_putchar('\n');
 }
