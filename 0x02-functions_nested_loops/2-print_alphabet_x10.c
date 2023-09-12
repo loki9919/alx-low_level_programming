61ec926b3e3094a71d58270aa664565799336c55
lokmane rouijel <lokmane9919@gmail.com>
2023-09-12 15:26:37 -0700

    print alph X10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@ void print_alphabet_x10(void)
 {
 	char letters;
 	int i;
-	
+
 	i = 0;
 	while (i < 10)
 	{
@@ -19,6 +19,5 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 		i++;
-	}
-		
+	}		
 }
