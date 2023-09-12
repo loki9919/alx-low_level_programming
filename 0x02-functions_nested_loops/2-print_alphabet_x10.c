ffda81658a3dbf6a928b062450362d4ae7cfd716
lokmane rouijel <lokmane9919@gmail.com>
2023-09-12 14:59:13 -0700

    print alph X10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- *
+ *print_alphabet_x10 - print all alphabets X10
 */
 void print_alphabet_x10(void)
 {
-	char letters = 'a';
+	char letters;
 	int i;
 	
 	i = 0;
 	while (i < 10);
 	{
+		letters = 1;
 		while (letters <= 'z')
 		{
 			_putchar(letters);
