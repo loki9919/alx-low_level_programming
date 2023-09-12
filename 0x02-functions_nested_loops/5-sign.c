4da7d9e6271fe08728f75179140c5a6362c01e9d
lokmane rouijel <lokmane9919@gmail.com>
2023-09-12 16:53:46 -0700

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if digit negative ,positiv or is 0
+*/
+int print_sign(int n)
+
+{
+	if (n >= 0)
+	{
+		_putchar(+);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+	}
+	else
+	{
+		_putchar(-);
+	}
+	_putchar('\n');
+}
