8b112b63ae71075c5aa842fda505f7d354b599c0
lokmane rouijel <lokmane9919@gmail.com>
2023-09-13 14:06:38 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,21 +2,22 @@
 
 /**
  * print_to_98 -  prints all natural numbers from n to 98 .
+ * @n: check character.
 */
 void print_to_98(int n)
 {
 	if (n < 98)
 	{
+		_putchar(n + '0');
 		_putchar(',');
 		_putchar(' ');
-		_putchar(n + '0');
 		n++;
 	}
 	else
 	{
+		_putchar(n + '0');
 		_putchar(',');
 		_putchar(' ');
-		_putchar(n + '0');
 		n--;
 	}
 	_putchar('\n');
