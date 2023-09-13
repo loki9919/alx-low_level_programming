5624aaa0790b5f4d977bc18c4204485457a8bf6d
lokmane rouijel <lokmane9919@gmail.com>
2023-09-12 17:43:32 -0700

    absolut value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ * @n: the chatacter to check
+ * @i: i = n x -1
+ * Return: retern
+*/
+int _abs(int)
+
+{
+	int n;
+	int i;
+	{
+		if (n >= '0')
+		{
+			_putchar(n);
+			return (0)
+		}
+		i = -n;
+		if (n < '0')
+		{ 
+			_putchar(i);
+			return (-1);
+		}
+		_putchar('\n');
+	}
+}		
