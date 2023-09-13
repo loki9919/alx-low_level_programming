16d0b14131aff8d3e84c9396794c43f601670e39
lokmane rouijel <lokmane9919@gmail.com>
2023-09-12 19:04:38 -0700

    24h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,15 @@
+#inclued "main.h"
+
+/*
+ * jack_bauer - 
+*/
+void jack_bauer(void)
+{
+	i = 0 && i < 23;
+	j = 0 && j <= 59;
+	{
+		_putchar(i + '0');
+		_putchar(':');
+		_putchar(j + '0');
+	}
+}
