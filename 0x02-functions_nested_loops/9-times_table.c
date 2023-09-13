b47d6023b9ec34fa33dce57a917f133c7195c77e
lokmane rouijel <lokmane9919@gmail.com>
2023-09-13 07:15:41 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,19 +6,29 @@
 void times_table(void)
 {
 	int x = 0;
-	int y = 0;
-	int z;
-	while (x <= 9)
+	int y, row, col;
+
+	row = 0;
+	while (x <= 0)
 	{
 		y = 0;
-		{
-			z = x * y;
-			putchar(z % 10 + '0');
-			y++;
-		} 
+		col = 0;
 		while (y <= 9)
-		putchar('\n');
+		{
+			_putchar(x * y + '0');
+			col++;
+			if (col == 9)
+			{
+				_putchar('\n');
+				col == 0;
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
+		}
 		x++;
 	}
 }
-
