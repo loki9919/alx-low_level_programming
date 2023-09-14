4b8d954a988febf651cfeb81d0badc01d78a2b04
lokmane rouijel <lokmane9919@gmail.com>
2023-09-13 19:03:47 -0700

    print numbers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,6 @@ void print_times_table(int n)
 {
 	if (n < 0 || n > 15)
 	return;
-
 	int i, j, result;
 
 	for (i = 0; i <= n; i++)
@@ -16,8 +15,7 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			result = i * j;
-
-				if (j > 0)
+			if (j > 0)
 				{
 					_putchar(',');
 					_putchar(' ');
