99b8d901d0c53f8c887276e3ed44777debf2aa8b
lokmane rouijel <lokmane9919@gmail.com>
2023-09-13 18:07:42 -0700

    prints the n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *
+*/
+void print_times_table(int n);\
+{
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+	for (int x = 0; x <= n; x++)
+	{
+		for (int y = 0; y <= n; y++)
+		{
+			int z = x * y;
+	
+			if (y > 0)
+			{
+				printf(",")
+			}
+			if (z < 10)
+			{
+				printf("   ");
+			}
+			if (z > 10 && z < 100)
+			{
+				printf("  ");
+			}
+			printf("%d", z);
+		}
+		printf("\n");
+	}
+}
