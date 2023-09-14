a366bde34b9208b445b9bd79cee0e7f96a6ec190
lokmane rouijel <lokmane9919@gmail.com>
2023-09-13 19:25:54 -0700

    print numbers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,48 +3,48 @@
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
- */
+*/
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-	return;
+	int i, j, k;
 
-	int i;
-	int j;
-	int result;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			result = i * j;
-
-			if (j > 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-
-				if (result < 10)
+				k = j * i;
+				if (j == 0)
 				{
+					_putchar(k + '0');
+				}
+				else if (k < 10 && j != 0)
+				{
+					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
 				}
-				else if (result < 100)
+				else if (k >= 10 && k < 100)
 				{
-				_putchar(' ');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
-
-			if (result < 10)
-			{
-				_putchar('0' + result);
-			}
-			else
-			{
-				_putchar('0' + result / 10);
-				_putchar('0' + result %10);
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
