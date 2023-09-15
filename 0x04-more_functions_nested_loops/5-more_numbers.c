82a3d52664e226bb3394a514107a313ae04340d1
lokmane rouijel <lokmane9919@gmail.com>
2023-09-14 18:21:12 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,24 +5,17 @@
 */
 void more_numbers(void)
 {
-	char x = 0;
+	int i, j;
 
-	while (x <= 14)
+	for (i = 1; i <= 10; i++)
 	{
-		char y = 0;
-
-		while (y <= 10)
+		for (j = 0; j <= 14; j++)
 		{
-			if (x < 14)
-			{
-				_putchar(x);
-				x++;
-			}
-			else
-			{
-				_putchar('\n');
-			}
-			_putchar('\n');
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar (j % 10 + '0');
 		}
+		_putchar('\n');
 	}
 }
+
