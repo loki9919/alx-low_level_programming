8a98e46dff1fdb07cf6b13e6d7272b29df37c61a
lokmane rouijel <lokmane9919@gmail.com>
2023-09-14 18:50:37 -0700

    print line in terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,14 +6,18 @@
  */
 void print_line(int n)
 {
-	if (n > 0)
+	if (n <= 0)
 	{
-		_putchar('_');
-		n++;
 		_putchar('\n');
 	}
 	else
 	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
 		_putchar('\n');
 	}
 }
