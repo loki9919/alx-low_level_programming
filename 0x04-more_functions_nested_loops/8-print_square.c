306a69ce900dffbcb9e6d96b82b6e8697f3c5394
lokmane rouijel <lokmane9919@gmail.com>
2023-09-15 15:54:36 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,29 +6,21 @@
  */
 void print_square(int size)
 {
-	int x, y;
-
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
+		int x, y;
+
 		for (x = 0; x < size; x++)
 		{
-			for (y = 0; y <= x; y++)
+			for (y = 0; y < size; y++)
 			{
-				if (y < x)
-				{
-					_putchar('#');
-				}
-				else if(y == x)
-				{
-					_putchar('#');
-					_putchar('\n');
-				}
+				_putchar('#');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
