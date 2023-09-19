d06d3ce2649c85d297f754dd70c3791831f30e22
lokmane rouijel <lokmane9919@gmail.com>
2023-09-19 15:25:09 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,10 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] <= '\0'; i += 2)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		_putchar(str[i] % 2);
 	}
 	_putchar('\n');
 }
