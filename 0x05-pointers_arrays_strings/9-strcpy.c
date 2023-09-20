859e84d3726a9193e92f372e2d9bdcd18211f185
lokmane rouijel <lokmane9919@gmail.com>
2023-09-19 19:21:39 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -6,13 +6,13 @@
  * @src: source string
  * Return: pointer to dest
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int i;
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[i] = src[i]
+		dest[i] = src[i];
 	}
 	dest[i] = '\0';
 	return (dest);
