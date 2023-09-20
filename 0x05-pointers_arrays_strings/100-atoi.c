3e07ab7032a2757de97c05969620b01c78142c1f
lokmane rouijel <lokmane9919@gmail.com>
2023-09-19 19:56:57 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,18 +12,25 @@ int _atoi(char *s)
 	int sign = 1;
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] == ' ' || s[i] == '\f' || s[i] == '\r')
 	{
+		i++;
+	}
+
 	if (s[i] == '-')
 	{
 		sign = -1;
+		i++;
 	}
-	else if (s[i] >= '0' && s[i] <= '9')
+	else if (s[i] == '+')
 	{
-		result = result * 10 + (s[i] - '0');
-	}
-	i++;
+		i++;
 	}
 
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
 	return result * sign;
 }
