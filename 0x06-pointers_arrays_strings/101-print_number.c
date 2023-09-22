ebf4c62065a23c99f17c9e2c44bfc715c213c7c1
lokmane rouijel <lokmane9919@gmail.com>
2023-09-22 07:12:05 -0700

    print numbers chars

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,17 @@
  */
 void print_number(int n)
 {
+	unsigned int i;
+
+	i = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		i = -n;
 	}
 	if (n / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(i / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((i % 10) + '0');
 }
