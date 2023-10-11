5b89e5d9295ffca57a831aecd66c1eb2c676d0ef
lokmane rouijel <lokmane9919@gmail.com>
2023-10-10 18:33:31 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,10 @@ struct dog
 	float age;
 	char *owner;
 };
-
+/**
+ * dog_t - typdef for struct dog
+ */
+typdef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
