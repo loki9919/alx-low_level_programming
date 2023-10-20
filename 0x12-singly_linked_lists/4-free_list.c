0755bade400f8c6e2905b95c9509df876012d375
lokmane rouijel <lokmane9919@gmail.com>
2023-10-20 12:43:51 -0700

    frees a linked lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - free list_t list
+ * @head: the head of the linked list
+ * Return: always 0
+ */
+void free_list(list_t *head)
+{
+	list_t *c;
+
+	c = head;
+
+	while (c != NULL)
+	{
+		head = head->next;
+		free(c->str);
+		free(c);
+	}
+}
