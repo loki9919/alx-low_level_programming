51701b78c79fd8085b921d3410b1bf0ca7cfd314
lokmane rouijel <lokmane9919@gmail.com>
2023-12-18 18:45:31 -0800

    app text to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - program appends text at the end of a file
+ * @filename: filename
+ * @text_content: added content
+ * Return: 1 if the file exists, -1 if the fails does not exist
+ * or if it fails
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int letters;
+	int wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (letters = 0; text_content[letters]; letters++)
+			;
+
+		wr = write(fd, text_content, letters);
+
+		if (wr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+i	return (1);
+}
