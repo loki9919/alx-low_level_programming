c49fdfb1d07ff40e03c0726a6e74be64046ff374
lokmane rouijel <lokmane9919@gmail.com>
2023-12-18 18:27:49 -0800

    read textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - program reads a text file and prints the letters
+ * @filename: filename
+ * @letters: numbers of letters printed
+ * Return: numbers of letters printed. It fails, returns 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t rd, wr;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (buf == NULL)
+		return (0);
+
+	rd = read(fd, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
+
+	close(fd);
+
+	free(buf);
+
+	return (wr);
+}
